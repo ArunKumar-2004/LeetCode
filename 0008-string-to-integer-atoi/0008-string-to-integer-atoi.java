cb01f38050e7f258e7c8ee9aacac6ac5beac8860
ArunKumar-2004 <126384804+ArunKumar-2004@users.noreply.github.com>
2023-12-18 14:25:51 +0530

    Time: 2 ms (45.51%), Space: 41.2 MB (82.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,24 +1,26 @@
 class Solution {
     public int myAtoi(String s) {
-        s=s.trim();
-        if(s.length()==0)
+        if (s == null || s.isEmpty()) {
             return 0;
-        s=s.replaceAll("^\\s+", "");
+        }
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
+        }
         int index = 0;
         int sign = 1;
         if (s.charAt(index) == '-' || s.charAt(index) == '+') {
             sign = (s.charAt(index++) == '-') ? -1 : 1;
         }
-        long num = 0;
+        long result = 0;
         while (index < s.length() && Character.isDigit(s.charAt(index))) {
-            num=num*10+(s.charAt(index++)-'0');
-            if(num*sign>Integer.MAX_VALUE){
+            result = result * 10 + (s.charAt(index++) - '0');
+            if (result * sign > Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
-            }else if(num*sign<Integer.MIN_VALUE){
+            } else if (result * sign < Integer.MIN_VALUE) {
                 return Integer.MIN_VALUE;
             }
         }
-        
-        return (int)num*sign;
+        return (int) (result * sign);
     }
 }
