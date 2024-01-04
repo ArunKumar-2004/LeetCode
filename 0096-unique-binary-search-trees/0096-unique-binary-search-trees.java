b63a6c511e260b70dc8f3ad755941543c9b5a9ca
ArunKumar-2004 <126384804+ArunKumar-2004@users.noreply.github.com>
2024-01-04 09:33:59 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (8.37%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -3,10 +3,13 @@ class Solution {
         if(n==0 || n==1){
             return 1;
         }
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            sum+=numTrees(i-1)*numTrees(n-i);
+        int sum[]=new int[n+1];
+        sum[0]=sum[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                sum[i]+=sum[j-1]*sum[i-j];
+            }
         }
-        return sum;
+        return sum[n];
     }
 }
