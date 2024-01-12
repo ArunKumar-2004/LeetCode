aee016870f55ae1cf9634ba4be9b4155fa9bedb5
ArunKumar-2004 <126384804+ArunKumar-2004@users.noreply.github.com>
2024-01-12 09:01:27 +0530

    Time: 3 ms (88.24%), Space: 42 MB (17.05%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,4 +1,10 @@
 class Solution {
+    public boolean isVowel(char ch){
+        if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+                return true;
+            }
+        return false;
+    }
     public boolean halvesAreAlike(String s) {
         if(s.length()==0){
             return false;
@@ -8,13 +14,11 @@ class Solution {
         int count[]={0,0};
         for(int i=0;i<sub1.length();i++){
             char ch=sub1.charAt(i);
-            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            if (isVowel(ch)) {
                 count[0]++;
             }
-        }
-        for(int i=0;i<sub2.length();i++){
-            char ch=sub2.charAt(i);
-            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            char ch1=sub2.charAt(i);
+            if (isVowel(ch1)) {
                 count[1]++;
             }
         }
