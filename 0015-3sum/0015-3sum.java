98b5467e99990b5c13788ee36d8bfd7090b8b06c
ArunKumar-2004 <126384804+ArunKumar-2004@users.noreply.github.com>
2024-01-24 10:18:21 +0530

    Time: 1004 ms (6.32%), Space: 56.1 MB (5.31%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        HashSet<String> set=new HashSet<>();
+        List<List<Integer>> li=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int curr=nums[i]+nums[j];
+                if(map.containsKey(-1*curr)){
+                    int k=map.get(-1*curr);
+                    String st=nums[i]+":"+nums[j]+":"+-1*curr;
+                    if(!set.contains(st) && i<k && j<k){
+                        li.add(Arrays.asList(nums[i], nums[j], -1*curr));
+                        set.add(st);
+                    }
+                }
+            }
+        }
+        return li;
+    }
+}
